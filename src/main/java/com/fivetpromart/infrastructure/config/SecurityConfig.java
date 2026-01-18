d4479fad1075f8baca5bc157dbc1a3e511f85247
Võ Trung Tín <tinvo2005@gmail.com>
2026-01-18 22:31:52 +0700

    Update SecurityConfig.java

--- src/main/java/com/fivetpromart/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/fivetpromart/infrastructure/config/SecurityConfig.java
@@ -37,12 +37,12 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**", "/error", "/actuator/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/api/v1/signup/**", "/error", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
-                                .jwtAuthenticationConverter(jwtAuthConverter) // ✅ Apply custom converter
+                                .jwtAuthenticationConverter(jwtAuthConverter) // Apply custom converter
                         )
                 );
 
